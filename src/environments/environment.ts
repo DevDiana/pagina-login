1424fc91fcea04fdb35039ed1b441fca3ba4deee
Diana Regina <devdianaregina@gmail.com>
2023-07-15 12:55:07 -0300

    iniciando a página de login

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: 'SEU_DOMINIO.firebaseapp.com',
+    projectId: 'SEU_ID_DO_PROJETO',
+    storageBucket: 'SEU_BUCKET.appspot.com',
+    messagingSenderId: 'SEU_SENDER_ID',
+    appId: 'SEU_APP_ID'
+  }
+};
